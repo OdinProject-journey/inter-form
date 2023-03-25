7675df8cd32225059f9ab5e5a2fcee237b15b310
AmanKhys <amankhys2112@gmail.com>
2023-03-25 13:48:01 +0530

    make layout with css next: confirm password with js

---
+++ script.js
@@ -0,0 +1,10 @@
+// const OGpass = document.getElementById("password");
+// const OGpassTest = OGpass.innerText;
+// const ConfirmPass = document.getElementById("ConfirmPassword").innerText;
+// const button = document.querySelector("button");
+
+// OGpass.addEventListener("click",()=>{
+//   if(OGpassTest != ConfirmPass){
+//     alert("pasword not matching");
+//   }
+// })
